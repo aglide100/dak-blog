83ddda032ab5031ae63f79a146ee00719aa19f72
aglide100 <aglide100@gmail.com>
2021-02-13 17:39:52 +0900

    config index.js => index.tsx

---
+++ ui/pages/_app.tsx
@@ -0,0 +1,18 @@
+// import React from "react"
+// import type { AppProps } from "next/app";
+import '../styles/globals.css'
+
+// type MyAppProps = {
+//   myAppProps: AppProps;
+// };
+
+function MyApp() {
+  // const { Component, pageProps } = props.myAppProps;
+
+  return <div>
+    Hello you can see MyApp Component
+  </div>
+  // return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ ui/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hello</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        Hello
+      </div>
+
+    </div>
+  )
+}
