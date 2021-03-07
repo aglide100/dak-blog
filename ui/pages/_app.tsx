ff12e06819cefcb69f2e6a4de4d92ee15bc3ac10
aglide100 <aglide100@gmail.com>
2021-03-07 21:57:39 +0900

    add some stuff

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { Header } from "../src/components/Header/Header";
+import { Header, HeaderProps } from "../src/components/Header/Header";
 import { Footer } from "../src/components/Footer/Footer";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const headerProps: HeaderProps = {
+    signIn: () => {
+      alert("wip");
+    },
+    signUp: () => {
+      alert("wip");
+    },
+    onClickGoToHome: () => {
+      router.push("/");
+    },
+  };
   return (
-    <div>
-      <Header></Header>
+    <div className="flex flex-col h-screen">
+      <Header {...headerProps}></Header>
       <Component {...pageProps} />
       <Footer></Footer>
     </div>
--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 
-export type HeaderProps = {};
+export type HeaderProps = {
+  signIn: () => void;
+  signUp: () => void;
+  onClickGoToHome: () => void;
+};
 
 export function Header(props: HeaderProps) {
   return (
-    <nav className="w-screen">
-      <div className="flex justify-between">
-        <div>Logo</div>
+    <nav className="w-screen h-24">
+      <div className="flex justify-between items-center">
+        <div
+          onClick={(ev) => {
+            ev.preventDefault();
+            props.onClickGoToHome();
+          }}
+        >
+          Logo
+          <img src=""></img>
+        </div>
 
-        <div>
-          <div>Sing In</div>
-          <div>Sing Up</div>
+        <div className="flex flex-row mr-10">
+          <div className="mr-4">
+            <button
+              onClick={(ev) => {
+                ev.preventDefault();
+                props.signIn();
+              }}
+            >
+              Sign In
+            </button>
+          </div>
+          <div className="mr-4">
+            <button
+              onClick={(ev) => {
+                ev.preventDefault();
+                props.signUp();
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
       </div>
     </nav>
