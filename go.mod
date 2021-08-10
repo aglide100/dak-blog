6b853688ff3ffa669e1395b15a7fcccd3b52de53
aglide100 <aglide100@gmail.com>
2021-08-10 19:35:37 +0900

    write protobuf sample

--- go.mod
+++ go.mod
@@ -4,9 +4,6 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.5.2
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
-	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67 // indirect
-	google.golang.org/grpc v1.39.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -8,14 +8,16 @@ import (
 	"github.com/golang/protobuf/proto"
 
 	"github.com/aglide100/personel-blog/pkg/models"
+
+	pb "github.com/aglide100/personel-blog/pkg/pb/unit"
 )
 
 const (filename = "person")
 
 func TestHttpRequest(resp http.ResponseWriter, req *http.Request) {
 	log.Printf("api: receive http xml request!")
-
-	person := pb.person{Color:"aaa"}
+	
+	person := &pb.Person{Name: "apple", Age: "12"}
 	out, err := proto.Marshal(person)
 	if err != nil {
 		log.Fatalf("Failed to write buff %v", err)
@@ -28,7 +30,7 @@ func TestHttpRequest(resp http.ResponseWriter, req *http.Request) {
 	// 	http.Error(resp, err.Error(), http.StatusInternalServerError)
 	// }
 	// resp.Write(a)
-}
+}  
  
 func SignIn(resp http.ResponseWriter, req *http.Request) {
 	log.Printf("api: receive Login User")
